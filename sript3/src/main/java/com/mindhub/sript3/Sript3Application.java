f255664493624feb8c513c2ebbaf73c3857ceacf
Bruno Trinitario <brunotrinitario@gmail.com>
2025-01-10 14:12:41 -0300

    Sprint3

---
+++ sript3/src/main/java/com/mindhub/sript3/Sript3Application.java
@@ -0,0 +1,13 @@
+package com.mindhub.sript3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sript3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sript3Application.class, args);
+	}
+
+}
---
+++ sript3/src/test/java/com/mindhub/sript3/Sript3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.sript3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sript3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
