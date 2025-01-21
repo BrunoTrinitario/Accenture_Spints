95f11751281f781a92103f6495d64d6601cce700
Bruno Trinitario <brunotrinitario@gmail.com>
2025-01-21 11:27:12 -0300

    First imports

--- sprint2/src/main/java/com/Sprint2/sprint2/dtos/TaskRecord.java
+++
@@ -1,7 +0,0 @@
-package com.Sprint2.sprint2.dtos;
-
-import com.Sprint2.sprint2.model.TaskStatus;
-
-public record TaskRecord(Long id, String title, String description, TaskStatus status) {
-
-}
--- sprint2/src/main/java/com/Sprint2/sprint2/model/TaskStatus.java
+++
@@ -1,7 +0,0 @@
-package com.Sprint2.sprint2.model;
-
-public enum TaskStatus {
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED;
-}
